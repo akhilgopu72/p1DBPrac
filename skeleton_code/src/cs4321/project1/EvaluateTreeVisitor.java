f31ac50a1d125178cf935cfaa0836e24692f19d4
Akhil <akhil@Akhils-MacBook-Pro.local>
2017-08-27 15:55:07 -0400

    src/

--- skeleton_code/src/cs4321/project1/EvaluateTreeVisitor.java
+++ skeleton_code/src/cs4321/project1/EvaluateTreeVisitor.java
@@ -10,18 +10,22 @@ import cs4321.project1.tree.UnaryMinusTreeNode;
 /**
  * Provide a comment about what your class does and the overall logic
  * 
- * @author Rong Tan (rt389)
+ * @author Rong Tan (rt389) and Akhil Gopu (akg68)
  */
 
 public class EvaluateTreeVisitor implements TreeVisitor {
-
+	private int result;
+	private Stack aStack;
+	
 	public EvaluateTreeVisitor() {
+		result = 0;
+		aStack = new Stack();
 		// TODO fill me in
 	}
 
 	public double getResult() {
 		// TODO fill me in
-		return 42; // so that skeleton code compiles
+		return result; // so that skeleton code compiles
 	}
 
 	@Override
@@ -31,6 +35,8 @@ public class EvaluateTreeVisitor implements TreeVisitor {
 
 	@Override
 	public void visit(UnaryMinusTreeNode node) {
+		if (node.getChild() instanceof LeafTreeNode)
+			aStack.push(((LeafTreeNode)node.getChild()).getData() * -1.0);
 		// TODO fill me in
 	}
 
